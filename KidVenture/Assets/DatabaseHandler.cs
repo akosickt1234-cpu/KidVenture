9b1abc9838592d33fc6b03101ee6c746a65fb4bf
WhyNotDevWND <imwhynotdeveloper@gmail.com>
2026-09-12 10:20:23 +0800

    Polished some code and implemented back feature

--- KidVenture/Assets/DatabaseHandler.cs
+++ KidVenture/Assets/DatabaseHandler.cs
@@ -5,49 +5,54 @@ using UnityEngine;
 using TMPro;
 //using UnityEngine.UIElements;
 using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class DatabaseHandler : MonoBehaviour
 {
     // The code will be a little bit messy but I will polish it after making the feature work
     public TextMeshProUGUI message;
     public TextMeshProUGUI greet;
-    public GameObject nameInput;
-    public GameObject ageIntput;
     public TMP_InputField namePlr;
     public TMP_InputField agePlr;
     public Button confirm;
     public GameObject addUserPanel;
 
-    // Checking if complete
-    void Start()
-    {
-        namePlr = nameInput.GetComponent<TMP_InputField>();
-        agePlr = ageIntput.GetComponent<TMP_InputField>();
+    public string userName;
+    public string age;
 
-        confirm.onClick.AddListener(ConfirmClick);
-    }
-
-    // Update is called once per frame
     public void Play()
     {
         addUserPanel.SetActive(true);
     }
 
-    void ConfirmClick()
+    public void ConfirmClick()
     {
-        if (namePlr.text != null && namePlr != null || agePlr.text != null && agePlr != null)
+        if (namePlr.text != null && namePlr != null)
         {
-            if (namePlr.text != null &&  agePlr.text != null)
-            {
-                message.text = "You are: " + namePlr.text + " | " + agePlr.text + " y.o.";
-                if (greet != null)
-                {
-                    greet.text = "Hello " + namePlr.text + "!";
-                }
-            }
+            userName = namePlr.text;
             
-
-            addUserPanel.SetActive(false);
         }
+
+        if (agePlr.text != null && agePlr != null)
+        {
+            age = agePlr.text;
+        }
+
+        if (userName != "" && age != "")
+        {
+            message.text = "You are: " + namePlr.text + " | " + agePlr.text + " y.o.";
+
+            if (greet != null)
+            {
+                greet.text = "Hello " + namePlr.text + "!";
+            }
+        }
+
+        addUserPanel.SetActive(false);
+    }
+
+    public void Back(GameObject Panel)
+    {
+        Panel.SetActive(false);
     }
 }
